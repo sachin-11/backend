2a5970dd6a0709367227900d0bb107b3777a71e8
sachin <sachin.singh@manektech.com>
2023-02-09 15:51:56 +0530

    init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
+app.get('/demo', (req, res) => {
     res.json({ message: 'Hello backend this is sachin..'})
 })
 
